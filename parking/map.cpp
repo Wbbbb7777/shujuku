4868ba85da84eadb03e47c3b87c147bde516b6d2
古埃及 <136501259+Wbbbb7777@users.noreply.github.com>
2023-06-14 11:07:23 +0800

    Delete parking directory

--- parking/map.cpp
+++
@@ -1,8 +0,0 @@
-#include "map.h"
-
-map::map(int mapWidth, int mapHeight, int tileWidth, int tileHeight){
-	this -> mapWidth = mapWidth;
-	this -> mapHeight = mapHeight;
-	this -> tileHeight = tileHeight;
-	this -> tileWidth = tileWidth;
-}
